0d5516ba0a1e1d9d0ed8f902e8d95de22e21a7bf
TheJhonXD <3227943820801@ingenieria.usac.edu.gt>
2023-02-12 12:31:21 -0600

    Tools

---
+++ Proyecto1/Tools.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jhonx on 12/02/23.
+//
+
+#include "Tools.h"
---
+++ Proyecto1/Tools.h
@@ -0,0 +1,8 @@
+//
+// Created by jhonx on 12/02/23.
+//
+
+#ifndef PROYECTO1_TOOLS_H
+#define PROYECTO1_TOOLS_H
+
+#endif //PROYECTO1_TOOLS_H
